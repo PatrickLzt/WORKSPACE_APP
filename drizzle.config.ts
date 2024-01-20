de59a030f91c19669d8eb4773d51eff3af0b2a95
Patrick Lorenzeti <patrick.lorenzeti@outlook.com>
2024-01-20 11:09:49 -0300

    feat: Add migrations and drizzle configuration files

---
+++ drizzle.config.ts
@@ -0,0 +1,25 @@
+/**
+ * @file drizzle.config.ts
+ * @author Patrick Lorenzeti <patrick.lorenzeti@mobitec.com.br>
+ * @brief Drizzle config
+ * @version 1.0
+ * @date 
+ *
+ */
+import type { Config } from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: '.env' })
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('Missing DRIZZLE_API_KEY')
+}
+
+export default {
+    schema: './src/lib/supabase/schema.ts',
+    out: './migrations',
+    driver: 'pg',
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL || '',
+    },
+} satisfies Config
