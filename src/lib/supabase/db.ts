9756c36e14e686fa642150d2dd67fa7bb0ad309b
Patrick Lorenzeti <patrick.lorenzeti@outlook.com>
2024-01-20 11:11:30 -0300

    fix: Paths for schema and ES issues

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,45 @@
+/**
+ * @file src/app/lib/supabase/db.ts
+ * @author Patrick Lorenzeti <patrick.lorenzeti@mobitec.com.br>
+ * @brief Database connection
+ * @version 1.0
+ * @date 
+ *
+ */
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as dotenv from 'dotenv'
+import * as schema from '../../../migrations/schema'
+import { migrate } from 'drizzle-orm/postgres-js/migrator'
+
+dotenv.config({ path: '.env' })
+
+if (!process.env.DATABASE_URL) {
+    throw new Error('Missing DATABASE_URL')
+}
+
+/**
+ * @brief Database connection
+ */
+const client = postgres(process.env.DATABASE_URL, { max: 1 })
+
+/**
+ * @brief Create database instance
+ */
+const db = drizzle(client, { schema })
+
+/**
+ * @brief Migrate database with schema
+ */
+const migrateDb = async () => {
+    try {
+        console.log('Migrating client...')
+        await migrate(db, { migrationsFolder: 'migrations' })
+        console.log('Database migrated successfully')
+    } catch (error) {
+        console.error('Error migrating client', error)
+    }
+}
+migrateDb()
+
+export default db
